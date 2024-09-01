0304e641bbaf9ab3ea96a9c9436325acf7f96122
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-09-01 17:57:08 +1000

    removed header emoji

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-primary text-white py-4">
       <div className="container mx-auto max-w-4xl text-center">
-        <h1 className="text-4xl font-bold mt-4 ">S2L - Real-Time Translator ✨</h1>
+        <h1 className="text-4xl font-bold mt-4 ">S2L - Real-Time Translator</h1>
         <p className="text-lg mt-2">Instantly translate spoken language in real-time 🧞</p>
       </div>
     </header>
